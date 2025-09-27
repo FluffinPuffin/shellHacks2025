4da837540b69fc941c022aab82f6d8dd108c1c8c
Chloe <chloechina04@aol.com>
2025-09-27 01:10:56 -0400

    Added title and logout message to logout.php

---
+++ frontEnd/auth.php
@@ -0,0 +1,14 @@
+<? session_start()?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <title> </title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+</body>
+</html>
---
+++ frontEnd/login.php
@@ -0,0 +1,18 @@
+<? session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <title> Login Page </title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <?
+
+
+    ?>
+</body>
+</html>

6031ee2ce2cff3b8412068cf1673ba6cdf7e28de
AfroTeddy12 <123010423+AfroTeddy12@users.noreply.github.com>
2025-09-27 18:27:21 -0400

    Add files via upload

---
+++ shellHacks2025/frontEnd/logout.php
@@ -0,0 +1,33 @@
+<?php
+// Start session
+session_start();
+// Not logged in, then go home
+if (!isset($_SESSION['username'])){
+    header("Location: home.php");
+}
+// If logged in then log out
+if(isset($_SESSION['username'])) {
+	unset($_SESSION['username']);
+    session_destroy();
+    header("Location: home.php");
+}
+ header("Location: home.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <title> Logout Page </title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div id = logout>
+        <h1> Logout Page </h1>
+        <p> Logging you out.... <p>
+        <a href="home.php"> Click here if you are not redirected </a>
+    </div>
+</body>
+</html>
